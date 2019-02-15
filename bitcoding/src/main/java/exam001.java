336208d1be3a9d30aa2bbb3fce5029f8ee70d82b
myeongsu <oplu92@gmail.com>
2019-02-15 22:54:48 +0900

    2019.02.15 gitignore 추가

---
+++ bitcoding/src/main/java/exam001.java
@@ -0,0 +1,9 @@
+public class exam001 {
+    public static void main(String[] args) {
+        System.out.printf("abc");
+        System.out.print("de"+"fg");
+        System.out.println(34);
+        System.out.print(10+21);
+    }
+
+}
